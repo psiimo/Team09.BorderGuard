fa4b2df1d5bca618a2cb8b0cc2b7a9e1b7ae4c41
Priit Siimo <psiimo@itcollege.ee>
2011-11-26 13:04:08 +0200

    Lisasin vajalikud entitid.

---
+++ BorderGuard/src/main/java/itcollege/team09/entities/AdminAlluvus.java
@@ -0,0 +1,82 @@
+package itcollege.team09.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@Entity
+@RooToString
+@RooEntity
+public class AdminAlluvus {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String kommentaar;
+    private String avaja;
+    private Date avatud;
+    private String muutja;
+    private Date muudetud;
+    private String sulgeja;
+    private Date suletud;
+    private static final long serialVersionUID = 1L;
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getKommentaar() {
+		return kommentaar;
+	}
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+	public String getAvaja() {
+		return avaja;
+	}
+	public void setAvaja(String avaja) {
+		this.avaja = avaja;
+	}
+	public Date getAvatud() {
+		return avatud;
+	}
+	public void setAvatud(Date avatud) {
+		this.avatud = avatud;
+	}
+	public String getMuutja() {
+		return muutja;
+	}
+	public void setMuutja(String muutja) {
+		this.muutja = muutja;
+	}
+	public Date getMuudetud() {
+		return muudetud;
+	}
+	public void setMuudetud(Date muudetud) {
+		this.muudetud = muudetud;
+	}
+	public String getSulgeja() {
+		return sulgeja;
+	}
+	public void setSulgeja(String sulgeja) {
+		this.sulgeja = sulgeja;
+	}
+	public Date getSuletud() {
+		return suletud;
+	}
+	public void setSuletud(Date suletud) {
+		this.suletud = suletud;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
---
+++ BorderGuard/src/main/java/itcollege/team09/entities/AdminYksus.java
@@ -0,0 +1,110 @@
+package itcollege.team09.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@Entity
+@RooToString
+@RooEntity
+public class AdminYksus {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String kood;
+	private String nimetus;
+    private String kommentaar;
+    private Date alates;
+    private Date kuni;
+    private String avaja;
+    private Date avatud;
+    private String muutja;
+    private Date muudetud;
+    private String sulgeja;
+    private Date suletud;
+    private static final long serialVersionUID = 1L;
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getKood() {
+		return kood;
+	}
+	public void setKood(String kood) {
+		this.kood = kood;
+	}
+	public String getNimetus() {
+		return nimetus;
+	}
+	public void setNimetus(String nimetus) {
+		this.nimetus = nimetus;
+	}
+	public String getKommentaar() {
+		return kommentaar;
+	}
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+	public Date getAlates() {
+		return alates;
+	}
+	public void setAlates(Date alates) {
+		this.alates = alates;
+	}
+	public Date getKuni() {
+		return kuni;
+	}
+	public void setKuni(Date kuni) {
+		this.kuni = kuni;
+	}
+	public String getAvaja() {
+		return avaja;
+	}
+	public void setAvaja(String avaja) {
+		this.avaja = avaja;
+	}
+	public Date getAvatud() {
+		return avatud;
+	}
+	public void setAvatud(Date avatud) {
+		this.avatud = avatud;
+	}
+	public String getMuutja() {
+		return muutja;
+	}
+	public void setMuutja(String muutja) {
+		this.muutja = muutja;
+	}
+	public Date getMuudetud() {
+		return muudetud;
+	}
+	public void setMuudetud(Date muudetud) {
+		this.muudetud = muudetud;
+	}
+	public String getSulgeja() {
+		return sulgeja;
+	}
+	public void setSulgeja(String sulgeja) {
+		this.sulgeja = sulgeja;
+	}
+	public Date getSuletud() {
+		return suletud;
+	}
+	public void setSuletud(Date suletud) {
+		this.suletud = suletud;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
